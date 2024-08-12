5b207ee936d6fce91867af5e7a68346c2b69c644
macmele <macmelejames@gmail.com>
2024-08-12 16:01:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
+
+
+function openTab(event, tabName) {
+    var i, tabContent, tabLinks;
+    
+    // Hide all tab content
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    // Remove the active class from all tab links
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+// Open the first tab by default
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.tab-link').click();
+});
